f80dff527e2a316b4dd27fc60d4fa9da0fb02411
raghuck <raghu.kumar.ck@gmail.com>
2023-12-11 21:50:00 +0530

    added task5 modules learning files

---
+++ task-5/main.tf
@@ -0,0 +1,40 @@
+# https://registry.terraform.io/modules/claranet/vpc-modules/aws/latest/submodules/public-subnets?tab=outputs
+module "vpc" {
+  source  = "claranet/vpc-modules/aws//modules/vpc"
+  version = "1.1.1"
+
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+module "public_subnets" {
+  source  = "claranet/vpc-modules/aws//modules/public-subnets"
+  version = "1.1.1"
+
+  vpc_id                  = module.vpc.vpc_id
+  gateway_id              = module.vpc.internet_gateway_id
+  map_public_ip_on_launch = true
+  cidr_block              = var.public_subnet_cidr_block
+  subnet_count            = 1
+  availability_zones      = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+# https://registry.terraform.io/modules/terraform-aws-modules/ec2-instance/aws/latest
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.5.0"
+
+  name = var.ec2_instance_name
+
+  instance_type          = var.instance_type
+  key_name               = var.ec2_user
+  monitoring             = true
+  vpc_security_group_ids = ["sg-02d2319e0a166341a"]
+  subnet_id              = module.public_subnets.subnet_ids.0
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
---
+++ task-5/variables.tf
@@ -0,0 +1,64 @@
+variable "vpc_cidr_block" {
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr_block" {
+    default = "10.0.1.0/24"
+}
+
+variable "ec2_instance_name" {
+    default = "nginx-instance"
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "ec2_user" {
+    default = "rapidd-aws-key"
+}
+
+#variable "subnet_cidr" {}
+#variable "availability_zone" {}
+#variable "subnet_name" {}
+
+#variable "route_table_name" {}
+
+#variable "internet_gateway_name" {}
+
+#variable "ami" {}
+#variable "instance_type" {}
+#variable "ec2_instance_name" {}
+
+variable "region" {
+  default     = "us-east-1"
+}
+
+variable "vpc_name" {
+  default     = "dev-vpc"
+}
+
+variable "public-subnet-name" {
+  default     = "pub-sub-1"
+}
+
+variable "private-subnet-name" {
+  default     = "prv-sub-1"
+}
+
+variable "route-table-name" {
+  default     = "dev-rt-1"
+}
+
+variable "internet-gateway-name" {
+  default     = "dev-igw-1"
+}
+
+#Tags
+variable "tag_env" {
+  default     = "dev"
+}
+
+variable "tag_project" {
+  default     = "smart-store"
+}
