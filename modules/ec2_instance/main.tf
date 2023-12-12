fba99cc4c728959066c8afc0cd562f6501318255
raghuck <raghu.kumar.ck@gmail.com>
2023-12-12 16:41:33 +0530

    removed unwanted folder

--- modules/ec2_instance/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "main" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
-
-  tags = {
-    Name = var.ec2_instance_name
-  }
-}
--- modules/subnet/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "main" {
-  vpc_id     = var.vpc_id
-  cidr_block = var.subnet_cidr
-  availability_zone = var.availability_zone
-
-  tags = {
-    Name = var.subnet_name
-  }
-}
